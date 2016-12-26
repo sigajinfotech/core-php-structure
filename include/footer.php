5d0521ec5d6dafc4fa85b943360c3610a35dc998
sigajinfotech <sigajinfotech@gmail.com>
2016-12-26 17:49:06 +0530

    commit changes in css and script

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
-	<script type="text/javascript" src="../js/jquery.min.js"></script>
-	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	
 	</body>
 </html>
